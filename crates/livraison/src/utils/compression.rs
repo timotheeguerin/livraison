bd9e08d18758c59b77bbc4443e1c0e3a043e3bbc
Timothee Guerin <tiguerin@microsoft.com>
2025-08-11 20:05:50 +0000

    use flate2

--- crates/livraison/src/utils/compression.rs
+++ crates/livraison/src/utils/compression.rs
@@ -1,11 +1,11 @@
 use std::io::Write;
 
-use libflate::gzip;
+use flate2::{Compression, write::GzEncoder};
 
 use crate::LivraisonResult;
 
 pub fn gzip(content: &[u8]) -> LivraisonResult<Vec<u8>> {
-    let mut encoder = gzip::Encoder::new(Vec::new())?;
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
     encoder.write_all(content)?;
-    Ok(encoder.finish().into_result()?)
+    Ok(encoder.finish()?)
 }
